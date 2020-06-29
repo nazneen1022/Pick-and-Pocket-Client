3c1e319fde74710e8d4abe6c33ecf8e30e9d69ac
nazneen1022 <nazneen1022@gmail.com>
2020-06-29 16:07:32 +0200

    removed extra slash at the end of the url

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = "https://pick-and-pocket-server.herokuapp.com/";
+export const apiUrl = "https://pick-and-pocket-server.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
