dc914defa076e742350826b3873b777f385b01b8
nazneen1022 <nazneen1022@gmail.com>
2020-07-05 18:44:07 +0200

    modified url to heroku url

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,3 +1,2 @@
-//export const apiUrl = "https://pick-and-pocket-server.herokuapp.com";
-export const apiUrl = "http://localhost:4000";
+export const apiUrl = "https://pick-and-pocket-server.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
